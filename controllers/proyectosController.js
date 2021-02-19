5c24a5c6cbe24423fcdbe6cf107196082c21fa9e
ImAleexDz <alexndro7145@gmail.com>
2021-02-18 21:18:56 -0600

    create routes and add form in new project

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,4 +1,35 @@
 exports.proyectosHome = (req, res) => {
     //Lo que se muestra en la página
-    res.render('index')
+    res.render('index', {
+        nombrePagina: 'Proyectos'
+    })
+}
+
+exports.formularioProyecto = (req, res) => {
+    res.render('nuevoProyecto', {
+        nombrePagina: 'Nuevo Proyecto'
+    })
+}
+
+exports.nuevoProyecto = (req, res) => {
+    //Enviar a la consola lo que el usuairo escriba
+    // console.log(req.body)
+
+    //validar que se tenga algo en el input
+    const { nombre } = req.body;
+    let errores = [];
+
+    if(!nombre) {
+        errores.push({
+            'texto': 'Agrega un nombre al proyecto'
+        })
+    }
+
+    //si hay errores pasar errores a la vista
+    if(errores.length > 0) {
+        res.render('nuevoProyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores
+        })
+    }
 }
--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const routes = require('./routes')
 //librería de express
 const path = require('path')
+const bodyParser = require('body-parser')
 
 //Crear una aplicación de express
 const app = express();
+
+//Donde cargar los archivos estáticos (css)
+app.use(express.static('public'))
 
 //Habilitar pug
 app.set('view engine', 'pug')
 
 //Añadir la carpeta de las vistas
 app.set('views', path.join(__dirname, './views'))
+
+//Habilitar body parser para ller datos del formulario
+app.use(bodyParser.urlencoded({extended: true}))
 
 //Ruta para el home
 app.use('/', routes())
